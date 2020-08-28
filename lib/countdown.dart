4068729cfcebdb5fb9fd461af3fbe680529d8778
Simon Frost <simonhfrost@gmail.com>
2020-08-28 13:42:30 +0200

    Add state to countdown widget

--- lib/countdown.dart
+++ lib/countdown.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
-class Countdown extends StatelessWidget {
+class Countdown extends StatefulWidget {
   final String musicalKey;
 
   const Countdown({ @required this.musicalKey });
 
   @override
+  State<StatefulWidget> createState() => new _CountdownState();
+}
+
+class _CountdownState extends State<Countdown> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(musicalKey),
+        title: Text(widget.musicalKey),
       ),
-      body: Center(child: Text(this.musicalKey)),
+      body: Center(child: Text(widget.musicalKey)),
     );
   }
 }
