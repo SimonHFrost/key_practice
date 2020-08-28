ad43e378e7013a9f041de10ccb74c4837c4b88ad
Simon Frost <simonhfrost@gmail.com>
2020-08-28 12:06:22 +0200

    Extract row creation

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-  
+
   final String title;
 
   @override
@@ -33,6 +33,20 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  InkWell createRow(String key, style) {
+    return InkWell(
+      child: Row(
+        children: [
+          Expanded(
+            child: (Text(key,
+                style: style, textAlign: TextAlign.center)),
+          )
+        ],
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final display1Style = Theme.of(context).textTheme.display1;
@@ -41,17 +55,19 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(children: [Expanded(child: (Text('C', style: display1Style, textAlign: TextAlign.center)))]),
-          Row(children: [Expanded(child: (Text('G', style: display1Style, textAlign: TextAlign.center)))]),
-          Row(children: [Expanded(child: (Text('D', style: display1Style, textAlign: TextAlign.center)))]),
+      body: Material(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            createRow('C', display1Style),
+            createRow('G', display1Style),
+            createRow('D', display1Style),
 //          Text(
 //            '$_counter',
 //            style: display1Style,
 //          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
