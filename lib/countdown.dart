62ca141295d398ef3fbad02d1645d021ac34ab2e
Simon Frost <simonhfrost@gmail.com>
2020-08-28 14:17:51 +0200

    Fix memory leak by canceling timer

--- lib/countdown.dart
+++ lib/countdown.dart
@@ -1,22 +1,54 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Countdown extends StatefulWidget {
   final String musicalKey;
 
-  const Countdown({ @required this.musicalKey });
+  int timeLeft = 5;
+
+  Countdown({Key key, this.musicalKey}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => new _CountdownState();
 }
 
 class _CountdownState extends State<Countdown> {
+  Timer _timer;
+
+  _CountdownState() {
+    _timer = Timer.periodic(new Duration(seconds: 1), (timer) {
+      setState(() {
+        if (widget.timeLeft < 1) {
+          Navigator.pop(context);
+          return;
+        }
+
+        widget.timeLeft--;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.musicalKey),
       ),
-      body: Center(child: Text(widget.musicalKey)),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Time Remaining:', style: Theme.of(context).textTheme.display1),
+            Text(widget.timeLeft.toString(), style: Theme.of(context).textTheme.display1),
+          ],
+        ),
+      ),
     );
   }
 }
