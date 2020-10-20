8800daa0d21e27fc33d0a57fc472ab0f71233699
Simon Frost <simonhfrost@gmail.com>
2020-10-20 20:26:33 +0200

    Basic cubit functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 enum MusicalKeyEvent { addMusicalKey }
 
@@ -8,17 +10,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Key Practice',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: HomeView(),
-    );
+        title: 'Key Practice',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+        ),
+        home: BlocProvider<MusicCubit>(
+          create: (BuildContext context) => MusicCubit(),
+          child: HomeView(),
+        ));
   }
 }
 
 class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
-    return ListView(children: <Widget>[Text('A'), Text('B'), Text('C')]);
+    return BlocBuilder<MusicCubit, List<String>>(
+      builder: (context, List<String> keys) => Scaffold(
+        body: Column(
+          children: [
+            Column(children: keys.map((key) => Text(key)).toList()),
+            FlatButton(
+              onPressed: () {
+                context.bloc<MusicCubit>().addKey('A');
+              },
+              child: Text('A'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+class MusicCubit extends Cubit<List<String>> {
+  MusicCubit() : super([]);
+
+  void addKey(key) => emit(state..add(key));
 }
